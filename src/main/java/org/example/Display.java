6038ed3ef7b67a23ecd20c070cc298cf21c5e659
XYMOND LOUISSE M. ALCAZAR <alcazarxymondlouisse@gmail.com>
2026-02-04 08:22:07 +0800

    added methods for button layout and others.

--- src/main/java/org/example/Display.java
+++ src/main/java/org/example/Display.java
@@ -16,17 +16,15 @@ public class Display {
         frame = new JFrame();
         frame.setTitle("Simple Data Preprocessing Program");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(350, 500));
         frame.setVisible(true);
     }
 
     //main menu to be shown as the program runs.
     public void mainMenu() {
         String[] menuChoices = {
-                "", "",
-                "", "",
-                "", "",
-                "", "",
-                ""
+                "Sort", "Filter",
+                "Search", "Cluster"
         };
         JPanel panel = new JPanel();
         JLabel greetings = new JLabel("Welcome to My Program!\n Please Select an Option.");
@@ -38,47 +36,63 @@ public class Display {
         frame.pack();
         frame.setVisible(true);
     }
+
     private JPanel menuButtonPanel(String[] buttonNames) {
-        ArrayList<JButton> menuButtons = addButtons(buttonNames.length, buttonNames);
-        int rows = buttonNames.length;
-        int columns = 0;
-        if (buttonNames.length % 2 == 0) {
-            rows = buttonNames.length / 2;
-            columns = 2;
-        } else if (buttonNames.length % 3 == 0) {
-            rows = buttonNames.length / 3;
-            columns = 3;
-        }
-        GridLayout buttonLayout = new GridLayout(rows, columns);
+        ArrayList<JButton> menuButtons = addButtons(buttonNames);
+        GridLayout buttonLayout = new GridLayout(buttonNames.length, 2);
         buttonLayout.setVgap(15);
         buttonLayout.setHgap(15);
+
+        if (buttonNames.length > 6) {
+            if (buttonNames.length % 2 == 0) {
+                buttonLayout = buttonLayout(
+                        "vertical",
+                        buttonNames.length / 2, 2
+                );
+            } else if (buttonNames.length % 3 == 0) {
+                buttonLayout = buttonLayout(
+                        "horizontal",
+                        3, buttonNames.length / 2
+                );
+            } else {
+               buttonLayout = buttonLayout(
+                        "horizontal",
+                        5, buttonNames.length / 5
+                );
+            }
+        }
+
         JPanel panel = new JPanel(buttonLayout);
         for (JButton button : menuButtons) {
             panel.add(button);
         }
-        JPanel wrappedPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 50, 50));
-        wrappedPanel.add(panel);
-        return wrappedPanel;
+
+        return addWrapper(panel);
     }
 
     //displays tabular form of the dataset
-    public JPanel displayDatasetTable(String[] header, ArrayList<Dataset> dataset) {
+    //TODO: change to cater database.
+    public JPanel displayDatasetTable(String[] header, ArrayList<Dataset> dataset, String labelTxt) {
         String[] buttonNames = {
                 "Back", "Something"
         };
         JPanel mainPanel = new JPanel(new BorderLayout());
         JTable dataTable = initializeTable(header, dataset);
-        ArrayList<JButton> buttons = addButtons(buttonNames.length, buttonNames);
-        JPanel buttonPanel = new JPanel(new GridLayout(0, buttons.size()));
-        buttonPanel.add(buttonPanel);
-        buttonPanel.setPreferredSize(new Dimension(200,300));
+        ArrayList<JButton> buttons = addButtons(buttonNames);
+        JPanel buttonPanel = new JPanel(buttonLayout("horizontal", 1, buttons.size()));
         for (JButton button : buttons) {
             buttonPanel.add(button);
         }
-        JLabel label = new JLabel("Something to test");
+
+        JTable someTable = new JTable(new DefaultTableModel(header, 10));
+        JLabel label = new JLabel(labelTxt);
         mainPanel.add(label, BorderLayout.NORTH);
-        mainPanel.add(dataTable, BorderLayout.CENTER);
-        mainPanel.add(buttonPanel, BorderLayout.SOUTH);
+        mainPanel.add(someTable, BorderLayout.CENTER);
+        mainPanel.add(addWrapper(buttonPanel), BorderLayout.SOUTH);
+        Container pane = frame.getContentPane();
+        pane.add(mainPanel);
+        frame.pack();
+        frame.setVisible(true);
         return mainPanel;
     }
 
@@ -99,15 +113,57 @@ public class Display {
         return new JTable(model);
     }
 
-    //returns a list of buttons to be used in a certain UI parts.
-    private ArrayList<JButton> addButtons(int count, String[] buttonNames) {
+    //DISPLAY UTILITIES//
+    /*
+    The methods below are the utilities used for the different components of the display.
+    That's why it is set to private.
+     */
+
+    /**
+     * This method creates a button array to be used in different parts of the program.
+     * @param buttonNames String array of names for the buttons to be created.
+     * @return ArrayList of buttons to be used in a certain UI parts.
+     */
+    private ArrayList<JButton> addButtons(String[] buttonNames) {
         ArrayList<JButton> buttonList = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            JButton button = new JButton(buttonNames[i]);
+        for (String buttonName : buttonNames) {
+            JButton button = new JButton(buttonName);
             button.setPreferredSize(new Dimension(144, 60));
             buttonList.add(button);
         }
 
         return buttonList;
+    }
+
+    /**
+     * Creates a Gridlayout for the buttons
+     * @param orientation horizontal or vertical.
+     * @return GridLayout to be used in button JPanels.
+     */
+    private GridLayout buttonLayout(String orientation, int rows, int columns) {
+        GridLayout buttonLayout = new GridLayout();
+        if (orientation.equalsIgnoreCase("vertical")) {
+            buttonLayout.setRows(columns);
+            buttonLayout.setColumns(rows);
+        } else if (orientation.equalsIgnoreCase("horizontal")) {
+            buttonLayout.setRows(rows);
+            buttonLayout.setColumns(columns);
+        }
+
+        buttonLayout.setVgap(15);
+        buttonLayout.setHgap(15);
+
+        return buttonLayout;
+    }
+    /**
+     * Used for wrapping JPanels in a FlowLayout Panel to have a properly sized components inside the BorderLayout.
+     * @param panel JPanel Components that needed to be wrapped.
+     * @return JPanel with FlowLayout
+     */
+    private JPanel addWrapper(JPanel panel) {
+        JPanel wrapper = new JPanel(new FlowLayout(FlowLayout.CENTER, 50, 50));
+        wrapper.add(panel);
+
+        return wrapper;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,8 @@
 package org.example;
+
+import javax.xml.crypto.Data;
+import java.util.ArrayList;
+
 /**
  * General Processes to be made:
  * <br>1. Sorting (Including: by title, [by number of files], by file size, by date, [by downloads], by usability, and [by vote counts])
@@ -12,6 +16,11 @@ public class Main {
         Utilities utilities = new Utilities();
         Display display = new Display();
 
+        String[] header = {"Sample", "Dataset"};
+        Dataset ds = new Dataset();
+        ArrayList<Dataset> dsArray = new ArrayList<>();
+        dsArray.add(ds);
+//        display.displayDatasetTable(header,dsArray, "Some text");
         display.mainMenu();
     }
 }
